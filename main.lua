23767fe060d46d5f77979b6cbb38ea2a8fa550cc
Beth Werbaneth <werbae@rpi.edu>
2011-09-30 22:44:37 -0400

    added a way to remove gas lines

--- main.lua
+++ main.lua
@@ -1,16 +1,28 @@
 --TEAM FOUR MAIN
 
+local gas_nodes = {}
+gas_nodes.size = 0
+
 --background
 local background = display.newRect(0, 0, display.contentWidth, display.contentHeight)
 background:setFillColor(0, 0, 0)
 
-
---gas functions
 local function add_gas(event)
     local gas_node = display.newCircle(event.x, event.y, 25)
     gas_node:setFillColor(255, 250, 205)
+    gas_nodes[gas_nodes.size + 1] = gas_node
+    gas_nodes.size = gas_nodes.size + 1
+end
+
+local function erase_gas(event)
+    while gas_nodes.size > 0 do
+        display.remove(gas_nodes[gas_nodes.size])
+        gas_nodes.size = gas_nodes.size -1
+    end
 end
 
 
 --event listeners
 Runtime:addEventListener("touch", add_gas)
+
+Runtime:addEventListener("accelerometer", erase_gas)
