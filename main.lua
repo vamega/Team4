d8ba467338e03f654bd62c8a34590073642f6cbb
Beth Werbaneth <werbae@rpi.edu>
2011-10-02 13:04:56 -0400

    make barrels disappear two seconds after clicking, put a limit on amount of gas you can pour

--- main.lua
+++ main.lua
@@ -4,6 +4,7 @@ physics = require "physics"
 --initialize gasoline container
 local gas_nodes = {}
 gas_nodes.size = 0
+gas_nodes.capacity = 350
 --initialize barrel container
 barrels = {}
 barrels.size = 0
@@ -14,34 +15,53 @@ background:setFillColor(0, 0, 0)
 
 --THE BARREL ZONE
 barrel = {}
-function barrel:new(x, y)--constructor
-    local instance = {x=x, y=y, radius = 50}
+function barrel:new(x, y, i)--constructor
+    local instance = {x=x, y=y, i=i, radius = 50, dead = false}
     instance.image = display.newCircle(x, y, 50)
     instance.image:setFillColor(255, 0, 0)
     setmetatable(instance, {__index = barrel})
     return instance
 end
 
+local function kill_barrel(index)
+    display.remove(barrels[index].image)
+    table.remove(barrels, index)
+end
+
 function barrel:touch(event) --detonate barrel
-    self.image:setFillColor(math.random(0, 255), math.random(0, 255), math.random(0, 255))
+    if event.phase == "began" and self.dead == false then
+        self.dead = true
+        self.image:setFillColor(math.random(0, 255), math.random(0, 255), math.random(0, 255))
+        local myclosure = function() return kill_barrel(self.i) end
+        timer.performWithDelay(2000, myclosure)
+    end
 end
 
 local function load_barrel(x, y)
-    barrels[barrels.size+1] = barrel:new(x, y)
+    barrels[barrels.size + 1] = barrel:new(x, y, barrels.size + 1)
     barrels.size = barrels.size + 1
 end
 
 --THE GAS STATION
+gas_node = {}
+function gas_node:new(x, y)--constructor
+    local node = {x = x, y=y, radius = 15}
+    node.image = display.newCircle(x, y, 15)
+    node.image:setFillColor(255, 250, 205)
+    setmetatable(node, {__index = gas_node})
+    return node
+end
+
 local function add_gas(event)
-    local gas_node = display.newCircle(event.x, event.y, 15)
-    gas_node:setFillColor(255, 250, 205)
-    gas_nodes[gas_nodes.size + 1] = gas_node
-    gas_nodes.size = gas_nodes.size + 1
+    if gas_nodes.size < gas_nodes.capacity then
+        gas_nodes[gas_nodes.size+1] = gas_node:new(event.x, event.y)
+        gas_nodes.size = gas_nodes.size + 1
+    end
 end
 
 local function erase_gas(event)
     while gas_nodes.size > 0 do
-        display.remove(gas_nodes[gas_nodes.size])
+        display.remove(gas_nodes[gas_nodes.size].image)
         table.remove(gas_nodes, gas_nodes.size)
         gas_nodes.size = gas_nodes.size -1
     end
