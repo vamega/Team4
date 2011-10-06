8ffc6e5b4bdf79df3f22e9f159c042c80cbad6e8
Varun Madiath <vamega@gmail.com>
2011-10-05 22:10:26 -0400

    Added initial code for multi-touch zoom

--- main.lua
+++ main.lua
@@ -1,17 +1,124 @@
 sprite = require "sprite"
 physics = require "physics"
 math = require "math"
-
+utils = require "utils"
 crate = require "crate"
 explosives = require "explosives"
 local intro = require("levels/intro")
 local explosives = require("explosives")
+
+system.activate( "multitouch" )
 
 physics.start()
 --physics.setDrawMode("hybrid")
 physics.setGravity(0, 0)
 
 mainDisplay = display.newGroup()
+
+-- Code to Handle zoom on the mainDisplay
+-- create a table listener object for the background image
+function mainDisplay:touch( event )
+	local result = true
+
+	local phase = event.phase
+
+	local previousTouches = self.previousTouches
+
+	local numTotalTouches = 1
+	if ( previousTouches ) then
+		-- add in total from previousTouches, subtract one if event is already in the array
+		numTotalTouches = numTotalTouches + self.numPreviousTouches
+		if previousTouches[event.id] then
+			numTotalTouches = numTotalTouches - 1
+		end
+	end
+
+	if "began" == phase then
+		-- Very first "began" event
+		if ( not self.isFocus ) then
+			-- Subsequent touch events will target button even if they are outside the stageBounds of button
+			display.getCurrentStage():setFocus( self )
+			self.isFocus = true
+
+			previousTouches = {}
+			self.previousTouches = previousTouches
+			self.numPreviousTouches = 0
+		elseif ( not self.distance ) then
+			local dx,dy
+
+			if previousTouches and ( numTotalTouches ) >= 2 then
+				dx,dy = utils.calculateDelta( previousTouches, event )
+			end
+
+			-- initialize to distance between two touches
+			if ( dx and dy ) then
+				local d = math.sqrt( dx*dx + dy*dy )
+				if ( d > 0 ) then
+					self.distance = d
+					self.xScaleOriginal = self.xScale
+					self.yScaleOriginal = self.yScale
+					print( "distance = " .. self.distance )
+				end
+			end
+		end
+
+		if not previousTouches[event.id] then
+			self.numPreviousTouches = self.numPreviousTouches + 1
+		end
+		previousTouches[event.id] = event
+
+	elseif self.isFocus then
+		if "moved" == phase then
+			if ( self.distance ) then
+				local dx,dy
+				if previousTouches and ( numTotalTouches ) >= 2 then
+					dx,dy = utils.calculateDelta( previousTouches, event )
+				end
+	
+				if ( dx and dy ) then
+					local newDistance = math.sqrt( dx*dx + dy*dy )
+					local scale = newDistance / self.distance
+					print( "newDistance(" ..newDistance .. ") / distance(" .. self.distance .. ") = scale("..  scale ..")" )
+					if ( scale > 0 ) then
+						self.xScale = self.xScaleOriginal * scale
+						self.yScale = self.yScaleOriginal * scale
+					end
+				end
+			end
+
+			if not previousTouches[event.id] then
+				self.numPreviousTouches = self.numPreviousTouches + 1
+			end
+			previousTouches[event.id] = event
+
+		elseif "ended" == phase or "cancelled" == phase then
+			if previousTouches[event.id] then
+				self.numPreviousTouches = self.numPreviousTouches - 1
+				previousTouches[event.id] = nil
+			end
+
+			if ( #previousTouches > 0 ) then
+				-- must be at least 2 touches remaining to pinch/zoom
+				self.distance = nil
+			else
+				-- previousTouches is empty so no more fingers are touching the screen
+				-- Allow touch events to be sent normally to the objects they "hit"
+				display.getCurrentStage():setFocus( nil )
+
+				self.isFocus = false
+				self.distance = nil
+				self.xScaleOriginal = nil
+				self.yScaleOriginal = nil
+
+				-- reset array
+				self.previousTouches = nil
+				self.numPreviousTouches = nil
+			end
+		end
+	end
+
+	return result
+end
 
 --initialization
 barrels = explosives.barrels
@@ -82,5 +189,7 @@ local function on_enter_frame(event)
 end
 
 Runtime:addEventListener("enterFrame", on_enter_frame)
-Runtime:addEventListener("touch", explosives.add_gas)
+--Runtime:addEventListener("touch", explosives.add_gas)
 Runtime:addEventListener("accelerometer", explosives.erase_gas)
+
+mainDisplay:addEventListener( "touch", mainDisplay )
