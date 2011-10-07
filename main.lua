f62aa74351d0bd9bb431bb31916bea9bf30110dc
unknown <Phelan@.(none)>
2011-10-07 17:08:12 -0400

    water

--- main.lua
+++ main.lua
@@ -5,6 +5,8 @@ utils = require "utils"
 crate = require "crate"
 explosives = require "explosives"
 gas = require "gas"
+water = require "water"
+require "collisionmanager"
 --local intro = require("levels/intro")
 local explosives = require("explosives")
 
@@ -121,16 +123,22 @@ function mainDisplay:touch( event )
 	return false
 end
 
+
+
+
+
+
+
 --initialization
 barrels = explosives.barrels
 gas_nodes = gas.gas_nodes
+waters = water.waters
 
 --local intro_img = intro.background
 
 --background
 background = display.newImage("background.png")
 mainDisplay:insert(background)
-
 
 --load test level
 explosives.spawn_barrel(110, 110)
@@ -143,6 +151,8 @@ crate.spawn_crate(249, 400)
 crate.spawn_crate(198, 389)
 crate.spawn_crate(240, 451)
 crate.spawn_crate(70, 580)
+
+water.load_water(400, 400)
 
 --add invisible boundaries so that objects don't go offscreen
 local top_edge = display.newRect(0, 0, display.contentWidth, 10)
@@ -177,6 +187,8 @@ end
 --game loop
 local last_frame_time = 0
 local function on_enter_frame(event)
+    collisionManager = CollisionManagerBuilder:new()
+    collisionManager:addCollisionTables(gas.gas_nodes, water.waters)
 	local elapsed_time = (event.time - last_frame_time) / 1000
 	elapsed_time = math.min(0.2, elapsed_time)
 	
---
+++ water.lua
@@ -0,0 +1,28 @@
+module(..., package.seeall)
+explosives = require("explosives")
+
+--initialize water
+waters = {}
+waters.size = 0
+
+
+--make individual water
+water = {}
+
+function water:new(x, y, i)--constructor
+    local instance = {x=x, y=y, i=i, length = 40, height = 40}
+    water.name = "water"
+    instance.image = display.newRect(x, y, 70, 70)
+    instance.image:setFillColor(0, 0, 255)
+    physics.addBody(instance.image,"static", {bounce = 0.4} )
+    setmetatable(instance, {__index = water})
+    return instance
+end
+
+function load_water(x, y)
+    waters[waters.size + 1] = water:new(x, y, waters.size + 1)
+    waters.size = waters.size + 1
+end
+
+
+
