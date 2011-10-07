eb2c9a9205f9bef83a65512af5150359f01eed7f
Beth Werbaneth <werbae@rpi.edu>
2011-10-07 17:45:20 -0400

    whatevs

--- levels.lua
+++ levels.lua
@@ -4,16 +4,20 @@ sprite = require "sprite"
 flammable = require "flammable"
 explosives = require "explosives"
 crate = require "crate"
+water = require "water"
 
 module(..., package.seeall)
 
 function tutorial_level()
     crate.spawn_crate(400, 600)
     crate.spawn_crate(100, 100)
-    explosives.spawn_barrel(200, 200)
-    myText = display.newText("Draw a gas line between the crates", 0, 200, "Helvetica", 48)
+    explosives.spawn_barrel(150, 700)
+    explosives.spawn_barrel(100, 800)
+    myText = display.newText("Draw a gas line \nbetween the crates", 0, 200, "Helvetica", 48)
     myText:setTextColor(255, 255, 255)
-
+    myText2 = display.newText("If you mess up,\nshake the phone to\nreset the level", 0, 500, "Helvetica", 48)
+    water.load_water(300, 500)
+    
 end
 
 function level_one()
