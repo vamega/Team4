f6b6d55591a3dc5e72906e683cb47138e9bcd85c
Varun Madiath <vamega@gmail.com>
2011-10-12 08:15:25 -0400

    Fixed all issues with multi-touch. God I really hate the Corona API for multitouch!

--- mainDisplay.lua
+++ mainDisplay.lua
@@ -35,10 +35,16 @@ function mainDisplay:touch( event )
 	local previousTouches = self.previousTouches
     -- The number of fingers on the screen
     local numTotalTouches = 1
+    local last_time = 0
     
-    --local box = display.newRect( 0, 0, 200, 200)
-    --local debug2 = display.newText(numTotalTouches, 60, 30, "Times New Roman", 48)
-    --debug2:setTextColor(0, 0, 0)
+    --[[ Debugging code to test misclassigied touch ended events.
+    local prevTouches = 1
+    local box = display.newRect( 0, 0, 200, 200)
+    local debug2 = display.newText(prevTouches, 60, 30, "Times New Roman", 48)
+    local debug3 = display.newText(prevTouches, 0, 30, "Times New Roman", 48)
+    debug3:setTextColor(0, 0, 0)
+    debug2:setTextColor(0, 0, 0)
+    --]]
 
 	if ( previousTouches ) then
 		-- add in total from previousTouches, subtract one if event is already in the array
@@ -77,7 +83,7 @@ function mainDisplay:touch( event )
 					self.distance = d
 					self.xScaleOriginal = self.xScale
 					self.yScaleOriginal = self.yScale
-					print( "distance = " .. self.distance )
+					--print( "distance = " .. self.distance )
 				end
 			end
 		end
@@ -102,7 +108,7 @@ function mainDisplay:touch( event )
 				if ( dx and dy ) then
 					local newDistance = math.sqrt( dx*dx + dy*dy )
 					local scale = newDistance / self.distance
-					print( "newDistance(" ..newDistance .. ") / distance(" .. self.distance .. ") = scale("..  scale ..")" )
+					--print( "newDistance(" ..newDistance .. ") / distance(" .. self.distance .. ") = scale("..  scale ..")" )
 					if ( scale > 0 ) then
                         local tempXScale = self.xScaleOriginal * scale
                         local tempYScale = self.yScaleOriginal * scale
@@ -134,14 +140,39 @@ function mainDisplay:touch( event )
 			previousTouches[event.id] = event
         end
     elseif "ended" == phase or "cancelled" == phase then
-        if numTotalTouches == 1 then
-            levels.reset_edges()
+		--[[ The idea here is to see if this touch ended signal is generated after an event that had 2 fingers on the screen.
+        -- If it was then it was the extraneous event, generated when the second of two fingers stopped touching the screen.
+        -- This however didn't seem to fix the issue.
+        if numTotalTouches >= 2 then
+            prevTouches = numTotalTouches
         end
-		if self.isFocus then
+        
+        if numTotalTouches == 1 then
+            if prevTouches == 1 then
+                box:setFillColor(255,0,0)
+                levels.scrollTouch(event)
+            else
+                prevTouches = 1
+            end
+        end--]]
+        
+        --[[
+        Ugly hack, if a single-touch touch ended event is generated less than 200ms after a multitouch touch ended event,
+        ignore it as it's caused by the second finger leaving the screen.
+        --]]
+        if numTotalTouches >= 2 then
+            touch_time = system.getTimer()
+        elseif numTotalTouches == 1 and system.getTimer() - touch_time > 200 then
+            levels.scrollTouch(event)
+        end
+        
+        if self.isFocus then
 			if previousTouches[event.id] then
 				self.numPreviousTouches = self.numPreviousTouches - 1
 				previousTouches[event.id] = nil
 			end
+            
+            
 
 			if ( #previousTouches > 0 ) then
 				-- must be at least 2 touches remaining to pinch/zoom
@@ -163,12 +194,15 @@ function mainDisplay:touch( event )
 		end
 	end
     
-    --debug.text = numFingersDown
-    --debug:toFront()
-    
-    --debug2.text = numTotalTouches
-    --display.newRect()
-    --debug2:toFront()
+    --
+    --[[ Debugging code to test a possible way to detect misclassified touch ended events
+    -- A single finger touch ended event is generated after a multi-touch touch ended signal, resulting in code doing unintended things.
+    debug2.text = prevTouches
+    debug3.text = numTotalTouches
+    box:toFront()
+    debug2:toFront()
+    debug3:toFront()
+    --]]
     
 	return true
 end
