a6a6924c2aba00518bf038eb36e840a59ab38ad2
Beth Werbaneth <werbae@rpi.edu>
2011-10-12 02:24:30 -0400

    stupid newline

--- buttons.lua
+++ buttons.lua
@@ -25,12 +25,6 @@ help_btn = {}
 hint_btn = {}
 text_blurb = {}
 mode_btn = {}
-text = {}
-text[1] = "Click on the barrel \\n to detonate it. \\n \\n The force of the \\n explosion will move \\n nearby objects"
-text[2] = "Draw a gas line \n between the crate \n and barrel \n \n If you make a \n mistake, shake the \n phone to reset \n level"
-text[3] = "You can only \n detonate one barrel \n per level"
---text[4] = "Do your best!"
-text[5] = "Water will \n extinguish any \n burning objects"
 
 function gas_meter:new(x, y)
     local instance = {}
@@ -118,17 +112,47 @@ function help_btn:touch(event)
     end
 end
 
+--text = {}
+--text[1] = "Click on the barrel \\n to detonate it. \\n \\n The force of the \\n explosion will move \\n nearby objects"
+--text[2] = "Draw a gas line \n between the crate \n and barrel \n \n If you make a \n mistake, shake the \n phone to reset \n level"
+--text[3] = "You can only \n detonate one barrel \n per level"
+--text[4] = "Do your best!"
+--text[5] = "Water will \n extinguish any \n burning objects"
+
 function text_blurb:new(i)
     local instance = {}
     instance.image = display.newImage("HintScreen2.png")--display.newRect(0, 0, display.contentWidth, display.contentHeight)
     --instance.image:setFillColor(0, 0, 0)
+    instance.text = {}
     instance.image:addEventListener("touch", instance)
     --instance.title = display.newText("Hint:", display.contentWidth/2-50, 50, "Helvetica", 48)
-    if text[i]~=nil then
-        instance.text = display.newText(text[i], 50, 150, "Helvetica", 48)
+    if i==1 then
+        instance.text[1] = display.newText("Click on the barrel", 50, 150, "Helvetica", 48)
+        instance.text[2] = display.newText("to detonate it.", 50, 200, "Helvetica", 48)
+        instance.text[3] = display.newText("The force of the", 50, 250, "Helvetica", 48)
+        instance.text[4] = display.newText("explosion will move", 50, 300, "Helvetica", 48)
+        instance.text[5] = display.newText("nearby objects.", 50, 350, "Helvetica", 48)
+    elseif i==2 then
+        instance.text[1] = display.newText("Draw a gas line", 50, 150, "Helvetica", 48)
+        instance.text[2] = display.newText("between the crate", 50, 200, "Helvetica", 48)
+        instance.text[3] = display.newText("and barrel.", 50, 250, "Helvetica", 48)
+        instance.text[4] = display.newText("If you make a", 50, 300, "Helvetica", 48)
+        instance.text[5] = display.newText("mistake, shake the", 50, 350, "Helvetica", 48)
+        instance.text[6] = display.newText("phone to reset", 50, 400, "Helvetica", 48)
+        instance.text[7] = display.newText("level.", 50, 450, "Helvetica", 48)
+    elseif i==3 then
+        instance.text[1] = display.newText("You can only", 50, 150, "Helvetica", 48)
+        instance.text[2] = display.newText("detonate one barrel", 50, 200, "Helvetica", 48)
+        instance.text[3] = display.newText("per level.", 50, 250, "Helvetica", 48)
+    elseif i==5 then
+        instance.text[1] = display.newText("Water will", 50, 150, "Helvetica", 48)
+        instance.text[2] = display.newText("extinguish any", 50, 200, "Helvetica", 48)
+        instance.text[1] = display.newText("burning objects.", 50, 250, "Helvetica", 48)
     else
-        instance.text = display.newText("You're on your\nown!", 50, 150, "Helvetica", 48)
+        instance.text[1] = display.newText("You're on your", 50, 150, "Helvetica", 48)
+        instance.text[2] = display.newText("own!", 50, 200, "Helvetica", 48)
     end
+    
     instance.killed= false
     if barrel.barrel_lock == false then
         instance.changed_barrel = true
@@ -156,7 +180,10 @@ end
 function text_blurb:kill()
     if self.killed == false then
         self.image:removeSelf()
-        self.text:removeSelf()
+        table_size = table.getn(self.text)
+        for i=table_size, 1, -1 do
+            self.text[i]:removeSelf()
+        end
         self.killed = true
     end
 end
